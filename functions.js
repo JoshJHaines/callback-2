67e160efd8a37f03f94afcb3ea58a5979a2c6c26
JoshuaJHaines <joshjhaines3@gmail.com>
2021-05-12 10:03:09 -0400

    tests for filter passing

--- functions.js
+++ functions.js
@@ -1,45 +1,53 @@
 const identity = function (value) {
-  return value;
+	return value;
 };
 
 const indexOf = function (array, target) {
-  //array = [1], target = 1
-  var result = -1;
+	//array = [1], target = 1
+	var result = -1;
 
-  each(array, function (item, index) {
-    //item = 1
-    if (item === target && result === -1) {
-      result = index;
-      //result = 1
-    }
-  });
+	each(array, function (item, index) {
+		//item = 1
+		if (item === target && result === -1) {
+			result = index;
+			//result = 1
+		}
+	});
 
-  return result;
+	return result;
 };
 
 const each = function (collection, iterator) {
-  if (collection instanceof Array) {
-    for (let i = 0; i < collection.length; i++) {
-      iterator(collection[i], i, collection);
-    }
-  } else if (collection instanceof Object) {
-    for (let prop in collection) {
-      iterator(collection[prop], prop, collection);
-    }
-  }
+	if (collection instanceof Array) {
+		for (let i = 0; i < collection.length; i++) {
+			iterator(collection[i], i, collection);
+		}
+	} else if (collection instanceof Object) {
+		for (let prop in collection) {
+			iterator(collection[prop], prop, collection);
+		}
+	}
 };
 
 const map = function (collection, iterator) {
-  var result = [];
+	var result = [];
 
-  each(collection, function (element, index, array) {
-    result.push(iterator(element));
-  });
+	each(collection, function (element, index, array) {
+		result.push(iterator(element));
+	});
 
-  return result;
+	return result;
 };
 
-const filter = function (collection, callback) {};
+const filter = function (collection, callback) {
+	let nArr = [];
+	each(collection, function (item) {
+		if (callback(item)) {
+			nArr.push(item);
+		}
+	});
+  return nArr
+};
 //reject([1, 2, 3, 4, 5, 6], isEven);
 const reject = function (collection, callbackTest) {};
 
@@ -48,9 +56,9 @@ const uniq = function (array) {};
 const reduce = function (collection, iterator, accumulator) {};
 
 module.exports = {
-  filter,
-  reject,
-  uniq,
-  reduce,
-  map,
+	filter,
+	reject,
+	uniq,
+	reduce,
+	map,
 };
